4593434ca400965ffc5d360cf5edbccf6c88f7bf
wgaskell <w4yneG@gmail.com>
2014-12-17 00:13:51 +0000

    change bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -55,15 +55,6 @@ cat <<EOF >data.sql
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
-
---------------------------------------------------------
-
 --
 -- Table structure for table `user`
 --
